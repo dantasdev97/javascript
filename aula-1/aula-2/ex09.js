3dbe655b992beef0140b1ed09b9cd4b8f2cd6609
dantasdev97 <144571833+dantasdev97@users.noreply.github.com>
2023-11-02 17:52:59 +0000

    .

---
+++ aula-1/aula-2/ex09.js
@@ -0,0 +1,8 @@
+var vel = 98
+
+console.log(`velocidade do carro e ${vel}KM/h`)
+if(vel > 60) { //condicao simples!
+    console.log(`Você ultrapassou a velocidade permitida , MULTADO!`)
+}
+
+console.log(`Dirija sempre usando o cinto de segurança`)
